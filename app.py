b28c45f53b7da42b82fc48403e0c39bd917ed92f
alminb <89650633+alminb@users.noreply.github.com>
2021-09-20 14:55:14 -0400

    Add files via upload
    basic login without connecting to database

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+# This is a sample Python script.
 
-#creating the app
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from flask import Flask,request,render_template
+import pickle
+
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
-def home():
-    return "This is the homepage"
+@app.route('/')
+def hello_world():
+    return render_template("login.html")
+userdatabase={'test':'123'}
+admindatabase={'admin':'admin'}
+
+@app.route('/login_form',methods=['POST','GET'])
+def login():
+    nameIN=request.form['username']
+    passwordIN=request.form['password']
+    if nameIN in userdatabase:
+        if userdatabase[nameIN] != passwordIN:
+            return render_template('login.html', info='Password incorrect.')
+        else:
+            return render_template('home.html', userlevel='user')
+    elif nameIN in admindatabase:
+        if admindatabase[nameIN]!=passwordIN:
+            return render_template('login.html',info='Password incorrect.')
+        else:
+            return render_template('home.html',userlevel='admin')
+    else:
+         return render_template('login.html', info='Username does not exist.')
+
+if __name__ == '__main__':
+    app.run()
