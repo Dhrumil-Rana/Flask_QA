9c3839f01ec096278915c68bf2976d23a735f275
DhrumilRana1998 <dgr25@njit.edu>
2021-09-20 16:58:46 -0400

    Added the login.html

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__, template_folder='template/static')
 
 @app.route("/", methods=['GET'])
 def sign_in():
-    return render_template("login.html")
+    return flask.render_template("login.html")
 
 @app.route("/signin", methods=['GET'])
 def content():
     return "This is the signin page"
+
+app.run()
