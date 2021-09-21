e04a8e428fb2eea124c9e755ecef338d65ebed80
alminb <89650633+alminb@users.noreply.github.com>
2021-09-20 21:24:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from flask import Flask,request,render_template
 import pickle
 
