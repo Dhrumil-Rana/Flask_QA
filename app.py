ad34cf7e1877dcfff93074dcb15496a3b97f2f20
DhrumilRana1998 <dgr25@njit.edu>
2021-09-22 17:43:13 -0400

    Added the password hash and select database feature

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask,request,render_template
 from flask_sqlalchemy import SQLAlchemy
 import pickle
+import hashlib
 
 app = Flask(__name__)
 
 ENV = 'prod'
+select_database = 'dhrumil'
+
 #this is for localhost
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Dhrumil1998@@@localhost/gameApp'
+    if select_database == 'dhrumil':
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Dhrumil1998@@@localhost/gameApp'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:almin123@localhost/flaskwebsite490'
 # this one is for the heruko
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wkmpqniejsynrs:cb8e4d066de141ba34aec6df0e1bc47e2d5741383d1e541a9ffb4d9a230c347a@ec2-18-209-143-227.compute-1.amazonaws.com:5432/d52kv5vd4tka5f'
+    if select_database == 'dhrumil':
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wkmpqniejsynrs:cb8e4d066de141ba34aec6df0e1bc47e2d5741383d1e541a9ffb4d9a230c347a@ec2-18-209-143-227.compute-1.amazonaws.com:5432/d52kv5vd4tka5f'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://onagbacopzfapd:985b15068892b63537c9a10a74d74d6579c45f677b4cba87594a09806e78e14d@ec2-52-23-87-65.compute-1.amazonaws.com:5432/d29sd9q7h5fs67'
+
 #this is general
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -29,20 +39,20 @@ class accounts(db.Model):
         self.password = password
         self.role = role
 
+
 @app.route('/')
 def hello_world():
     return render_template("login.html")
 
+
 @app.route('/login',methods=['POST','GET'])
 def login():
     nameIN=request.form['username']
-    passwordIN=request.form['password']
+    passwordIN=request.form['password'].encode('utf-8')
 
-
-    #check if username is in the accounts database
     user = accounts.query.filter_by(username=nameIN).first()
     if user:
-        if user.password == passwordIN:
+        if user.password == hashlib.sha256(passwordIN).hexdigest():
             if user.role == 'U':
                 return render_template('home.html',name = nameIN, userlevel='user')
             else:
@@ -51,6 +61,7 @@ def login():
             return render_template('login.html',info='Password incorrect.')
     else:
          return render_template('login.html', info='Account with that username does not exist.')
+
 
 if __name__ == '__main__':
     app.run()
