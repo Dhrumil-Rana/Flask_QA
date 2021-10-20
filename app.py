6797bd09bee1c96372540c0cfcf2c4f7a8ec52a5
alminb <ab2334@njit.edu>
2021-10-20 14:56:55 -0400

    latest working version, working on adding comments

--- app.py
+++ app.py
@@ -139,8 +139,12 @@ def login():
 
 
 @app.route('/home', methods=['POST', 'GET'])
-def post():
-    return render_template('home.html',name=session.get('name'), userlevel=session.get('userlevel'))
+def home():
+    if request.method == 'GET':
+        post = posts.query.all()
+        comment = comments.query.all()
+
+        return render_template('home.html',name=session.get('name'), userlevel=session.get('userlevel'), posts=post, comments=comment)
 # return a list of post and gian has to make a css file such that it will show in sequence
 
 
