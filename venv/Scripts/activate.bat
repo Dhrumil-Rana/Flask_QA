092568ac0d854d983ff279b9484edc3c8dda3eaa
Dhrumil-Rana <dgr25@njit.edu>
2024-04-08 01:24:40 -0400

    Sync

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dhrum\Desktop\Dhrumil\School\Fourth semester\Cs 490\Repo\Flask_QA\venv
+set VIRTUAL_ENV=C:\Users\dhrum\OneDrive\Desktop\Dhrumil Rana\Github\Flask_QA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(venv) 
 
 :END
 if defined _OLD_CODEPAGE (
